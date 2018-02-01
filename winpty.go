2013a1ca105e4c0568189df1e56ce2a01f72b159
Pieter Lazzaro <pieter.lazzaro@pureharvest.com.au>
2018-02-01 14:51:45 -0500

    Fix bug on 386 and add options when launching agent.

--- winpty.go
+++ winpty.go
@@ -1,107 +1,125 @@
 package winpty
 
 import (
-    "os"
-    "fmt"
-    "syscall"
-    "unsafe"
+	"fmt"
+	"os"
+	"syscall"
+	"unsafe"
 )
 
-type WinPTY struct {
-    StdIn           *os.File
-    StdOut          *os.File
+type Options struct {
+	// DLLPrefix is the path to winpty.dll and winpty-agent.exe
+	DLLPrefix string
 
-    wp              uintptr
-    childHandle     uintptr
-    closed          bool
+	// AppName sets the title of the console
+	AppName string
+
+	// Command is the full command to launch
+	Command string
+
+	// Dir sets the current working directory for the command
+	Dir string
+
+	// Env sets the environment variables. Use the format VAR=VAL.
+	Env []string
+}
+
+type WinPTY struct {
+	StdIn  *os.File
+	StdOut *os.File
+
+	wp          uintptr
+	childHandle uintptr
+	closed      bool
 }
 
 // accepts path to command to execute, then arguments.
 // returns WinPTY object pointer, error.
 // remember to call Close on WinPTY object when done.
 func Open(dllPrefix, cmd string) (*WinPTY, error) {
-    setupDefines(dllPrefix)
+	return OpenWithOptions(Options{
+		DLLPrefix: dllPrefix,
+		Command:   cmd,
+	})
+}
 
-    var errorPtr uintptr
-    defer winpty_error_free.Call(errorPtr)
-    agentCfg, _, _ := winpty_config_new.Call(uintptr(uint32(0)), uintptr(unsafe.Pointer(errorPtr)))
-    if agentCfg == uintptr(0) {
-        return nil, fmt.Errorf("Unable to create agent config, %s", GetErrorMessage(errorPtr))
-    }
-    // Set the initial size to 40x40.
-    winpty_config_set_initial_size.Call(agentCfg, uintptr(uint32(40)), uintptr(uint32(40)))
+func OpenWithOptions(options Options) (*WinPTY, error) {
+	setupDefines(options.DLLPrefix)
 
-    var openErr uintptr
-    defer winpty_error_free.Call(openErr)
-    wp, _, _ := winpty_open.Call(agentCfg, uintptr(unsafe.Pointer(openErr)))
-    if wp == uintptr(0) {
-        return nil, fmt.Errorf("Error Launching WinPTY agent, %s", GetErrorMessage(openErr))
-    }
-    winpty_config_free.Call(agentCfg)
+	agentCfg, err := createAgentCfg(0)
 
-    stdin_name, _, _ := winpty_conin_name.Call(wp)
-    stdout_name, _, _ := winpty_conout_name.Call(wp)
+	if err != nil {
+		return nil, err
+	}
 
-    obj := &WinPTY{}
-    stdin_handle, err := syscall.CreateFile((*uint16)(unsafe.Pointer(stdin_name)), syscall.GENERIC_WRITE, 0, nil, syscall.OPEN_EXISTING, 0, 0)
-    if err != nil {
-        return nil, fmt.Errorf("Error getting stdin handle. %s", err)
-    }
-    obj.StdIn = os.NewFile(uintptr(stdin_handle), "stdin")
-    stdout_handle, err := syscall.CreateFile((*uint16)(unsafe.Pointer(stdout_name)), syscall.GENERIC_READ, 0, nil, syscall.OPEN_EXISTING, 0, 0)
-    if err != nil {
-        return nil, fmt.Errorf("Error getting stdout handle. %s", err)
-    }
-    obj.StdOut = os.NewFile(uintptr(stdout_handle), "stdout")
+	// Set the initial size to 40x40.
+	winpty_config_set_initial_size.Call(agentCfg, uintptr(uint32(40)), uintptr(uint32(40)))
 
-    cmdLineStr, err := syscall.UTF16PtrFromString(cmd)
-    if err != nil {
-        return nil, fmt.Errorf("Failed to convert cmd to pointer.")
-    }
-    spawnCfg, _, _ := winpty_spawn_config_new.Call(uintptr(uint64(WINPTY_SPAWN_FLAG_AUTO_SHUTDOWN)), uintptr(0), uintptr(unsafe.Pointer(cmdLineStr)), uintptr(0), uintptr(0), uintptr(0));
+	var openErr uintptr
+	defer winpty_error_free.Call(openErr)
+	wp, _, _ := winpty_open.Call(agentCfg, uintptr(unsafe.Pointer(openErr)))
 
-    var (
-        spawnErr    uintptr
-        lastError   *uint32
-    )
-    spawnRet, _, _ := winpty_spawn.Call(wp, spawnCfg, uintptr(unsafe.Pointer(obj.childHandle)), uintptr(0), uintptr(unsafe.Pointer(lastError)), uintptr(unsafe.Pointer(spawnErr)))
-    winpty_spawn_config_free.Call(spawnCfg)
-    defer winpty_error_free.Call(spawnErr)
+	if wp == uintptr(0) {
+		return nil, fmt.Errorf("Error Launching WinPTY agent, %s", GetErrorMessage(openErr))
+	}
 
-    if spawnRet == 0 {
-        return nil, fmt.Errorf("Error spawning process...")
-    } else {
-        obj.wp = wp
-        return obj, nil
-    }
+	winpty_config_free.Call(agentCfg)
+
+	stdin_name, _, _ := winpty_conin_name.Call(wp)
+	stdout_name, _, _ := winpty_conout_name.Call(wp)
+
+	obj := &WinPTY{}
+	stdin_handle, err := syscall.CreateFile((*uint16)(unsafe.Pointer(stdin_name)), syscall.GENERIC_WRITE, 0, nil, syscall.OPEN_EXISTING, 0, 0)
+	if err != nil {
+		return nil, fmt.Errorf("Error getting stdin handle. %s", err)
+	}
+	obj.StdIn = os.NewFile(uintptr(stdin_handle), "stdin")
+	stdout_handle, err := syscall.CreateFile((*uint16)(unsafe.Pointer(stdout_name)), syscall.GENERIC_READ, 0, nil, syscall.OPEN_EXISTING, 0, 0)
+	if err != nil {
+		return nil, fmt.Errorf("Error getting stdout handle. %s", err)
+	}
+	obj.StdOut = os.NewFile(uintptr(stdout_handle), "stdout")
+
+	spawnCfg, err := createSpawnCfg(WINPTY_SPAWN_FLAG_AUTO_SHUTDOWN, options.AppName, options.Command, options.Dir, options.Env)
+
+	if err != nil {
+		return nil, err
+	}
+
+	var (
+		spawnErr  uintptr
+		lastError *uint32
+	)
+	spawnRet, _, _ := winpty_spawn.Call(wp, spawnCfg, uintptr(unsafe.Pointer(obj.childHandle)), uintptr(0), uintptr(unsafe.Pointer(lastError)), uintptr(unsafe.Pointer(spawnErr)))
+	winpty_spawn_config_free.Call(spawnCfg)
+	defer winpty_error_free.Call(spawnErr)
+
+	if spawnRet == 0 {
+		return nil, fmt.Errorf("Error spawning process...")
+	} else {
+		obj.wp = wp
+		return obj, nil
+	}
 }
 
 func (obj *WinPTY) SetSize(ws_col, ws_row uint32) {
-    if ws_col == 0 || ws_row == 0 {
-        return
-    }
-    winpty_set_size.Call(obj.wp, uintptr(ws_col), uintptr(ws_row), uintptr(0))
+	if ws_col == 0 || ws_row == 0 {
+		return
+	}
+	winpty_set_size.Call(obj.wp, uintptr(ws_col), uintptr(ws_row), uintptr(0))
 }
 
 func (obj *WinPTY) Close() {
-    if obj.closed {
-        return
-    }
+	if obj.closed {
+		return
+	}
 
-    winpty_free.Call(obj.wp)
+	winpty_free.Call(obj.wp)
 
-    obj.StdIn.Close()
-    obj.StdOut.Close()
+	obj.StdIn.Close()
+	obj.StdOut.Close()
 
-    syscall.CloseHandle(syscall.Handle(obj.childHandle))
+	syscall.CloseHandle(syscall.Handle(obj.childHandle))
 
-    obj.closed = true
-}
-
-func GetErrorMessage(ptr uintptr) (string) {
-    msgPtr, _, _ := winpty_error_msg.Call(ptr)
-    if msgPtr == uintptr(0) {
-        return "Unknown Error"
-    }
-    return UTF16PtrToString((*uint16)(unsafe.Pointer(msgPtr)))
+	obj.closed = true
 }
